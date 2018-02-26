451d64046dc11ab1704f1d5ea053cc16712bf97a
amadarain <t.f.octo@gmail.com>
2018-02-27 08:32:38 +0900

    init Gradle project

---
+++ src/main/java/com/github/am4dr/gradle/java_modularize/GradleJavaModularizePlugin.java
@@ -0,0 +1,12 @@
+package com.github.am4dr.gradle.java_modularize;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GradleJavaModularizePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
