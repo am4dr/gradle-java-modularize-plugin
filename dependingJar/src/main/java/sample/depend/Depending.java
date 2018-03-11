c7089e3b2dc20b130ec2ff4f52cbcd2c2cdb2860
amadarain <t.f.octo@gmail.com>
2018-03-11 16:07:19 +0900

    add dependency library sample

---
+++ dependingJar/src/main/java/sample/depend/Depending.java
@@ -0,0 +1,12 @@
+package sample.depend;
+
+import sample.Unnamed;
+
+public class Depending {
+
+    public final Unnamed unnamed;
+
+    public Depending(Unnamed unnamed) {
+        this.unnamed = unnamed;
+    }
+}
