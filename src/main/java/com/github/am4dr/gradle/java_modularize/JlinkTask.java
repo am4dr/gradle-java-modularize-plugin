2bbad3d5e9bbb9db7160282641bd89844fc3279d
amadarain <t.f.octo@gmail.com>
2018-03-19 07:36:56 +0900

    use temp dir for JlinkTask

--- src/main/java/com/github/am4dr/gradle/java_modularize/JlinkTask.java
+++ src/main/java/com/github/am4dr/gradle/java_modularize/JlinkTask.java
@@ -2,13 +2,18 @@ package com.github.am4dr.gradle.java_modularize;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.Directory;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -23,29 +28,31 @@ public class JlinkTask extends DefaultTask {
     private Property<String> launchScriptName = objects.property(String.class);
     private Property<String> launcherClass = objects.property(String.class);
     private Property<Integer> compress = objects.property(Integer.class);
-    private DirectoryProperty outputDir = newOutputDirectory();
+    private Property<String> outputDirName = objects.property(String.class);
+    private DirectoryProperty outputParentDir = newOutputDirectory();
 
     public JlinkTask() {
         args.set(List.of());
+        outputParentDir.set(getProject().getLayout().getBuildDirectory().dir("jlink"));
+        outputDirName.set(launchScriptName);
+        compress.set(0);
     }
 
     @TaskAction
-    public void jlink() {
+    public void jlink() throws IOException {
+        final File tempDir = new File(getTemporaryDir(), outputDirName.get());
         if (args.get().isEmpty()) {
-            args.set(createArgs(modulePaths.getFiles(), modules.get(), launchScriptName.get(), launcherClass.get(), compress.get(), outputDir.get().getAsFile()));
+            args.set(createArgs(modulePaths.getFiles(), modules.get(), launchScriptName.get(), launcherClass.get(), compress.get(), tempDir));
         }
         final ToolProviderSupport.Result result = jlink(args.get());
         if (result.exitCode != 0) {
-            throw new TaskExecutionException(this, new IllegalStateException("exit code is not 0: " + result.err));
+            throw new TaskExecutionException(this, new IllegalStateException("exit code is not 0: " + result.out + "\n\n" + result.err));
         }
+        Files.move(tempDir.toPath(), getOutputDir().get().getAsFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static ToolProviderSupport.Result jlink(List<String> args) {
         return ToolProviderSupport.run("jlink", args);
-    }
-
-    public static ToolProviderSupport.Result jlink(Set<File> modulePaths, List<String> modules, String name, String launcherClassName, int compress, File outputDir) {
-        return jlink(createArgs(modulePaths, modules, name, launcherClassName, compress, outputDir));
     }
 
     private static ArrayList<String> createArgs(Set<File> modulePaths, List<String> modules, String name, String launcherClassName, int compress, File outputDir) {
@@ -88,8 +95,19 @@ public class JlinkTask extends DefaultTask {
         return compress;
     }
 
-    @OutputDirectory
-    public DirectoryProperty getOutputDir() {
-        return outputDir;
+    @Input
+    public Property<String> getOutputDirName() {
+        return outputDirName;
     }
+
+    @OutputDirectory
+    public DirectoryProperty getOutputParentDir() {
+        return outputParentDir;
+    }
+
+    @OutputDirectory
+    public Provider<Directory> getOutputDir() {
+        return outputParentDir.dir(outputDirName);
+    }
+
 }
